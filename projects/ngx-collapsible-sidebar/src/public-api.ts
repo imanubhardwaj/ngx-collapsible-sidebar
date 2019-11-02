fb3c6907cf01507b7283109cb8904eac11f5459e
Manu Bhardwaj <mnbhardwaj888@gmail.com>
2019-11-03 00:50:01 +0530

    [add] directives export

--- projects/ngx-collapsible-sidebar/src/public-api.ts
+++ projects/ngx-collapsible-sidebar/src/public-api.ts
@@ -3,8 +3,13 @@
  */
 
 export * from './lib/ngx-collapsible-sidebar.module';
+
 export * from './lib/components/ngx-collapsible-sidebar-container/ngx-collapsible-sidebar-container.component';
 export * from './lib/components/ngx-collapsible-sidebar-content/ngx-collapsible-sidebar-content.component';
 export * from './lib/components/ngx-collapsible-sidebar/ngx-collapsible-sidebar.component';
 export * from './lib/components/ngx-collapsible-sidebar-item/ngx-collapsible-sidebar-item.component';
 export * from './lib/components/ngx-collapsible-sidebar-sub-item/ngx-collapsible-sidebar-sub-item.component';
+
+export * from './lib/directives/hide-on-collapse.directive';
+export * from './lib/directives/collapsed-sidebar-item.directive';
+export * from './lib/directives/expanded-sidebar-item.directive';
